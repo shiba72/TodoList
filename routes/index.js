b3f82b96e8730f7d63d7bc3a2ff9bbf2cba065a3
barcarunrun <barcarunrunbiscabarca@gmail.com>
2020-02-19 10:39:03 +0900

    first

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+  //host: "127.0.0.1",
+　//port: 3307,
+  socketPath: `/cloudsql/${"oceanic-airway-268105:us-central1:root"}`,
+  user: "root",
+  password: "Kid1214",
+  database: "todo_db"
+});
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+module.exports = router;
